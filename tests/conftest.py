9f3013cbf7ae56f8ca03bff322fd48a048f95db2
Elina Mazitova <elinamazitova14@gmail.com>
2025-11-16 17:13:22 +0300

    Add browser_setup

---
+++ tests/conftest.py
@@ -0,0 +1,11 @@
+import pytest
+from selene import browser
+
+@pytest.fixture(autouse=True, scope='function')
+def browser_setup():
+    browser.config.base_url = 'https://demoqa.com'
+    browser.config.timeout = 6
+    browser.config.window_width = 1920
+    browser.config.window_height = 1080
+    yield
+    browser.quit()
